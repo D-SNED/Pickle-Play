ce90d5e39564582f96ec6dd308bab05b5f16d6bb
Christopher <cmbush@gmail.com>
2023-10-10 15:49:28 -0700

    added locations router

--- api/routers/locations.py
+++ api/routers/locations.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import List, Union
+from queries.locations import LocationOut, LocationRepository, Error
 
 
 router = APIRouter()
 
 
-@router.get("/api/locations/", response_model)
+@router.get("/api/locations/", response_model=Union[List[LocationOut], Error])
+def get_all(
+    repo: LocationRepository = Depends(),
+):
+    return repo.get_all()
